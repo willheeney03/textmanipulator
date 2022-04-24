fda80a25efae30e6e618524735f3b4b56b561ee0
willheeney03 <99232572+willheeney03@users.noreply.github.com>
2022-04-24 15:36:47 -0500

    yes

---
+++ TextManipulator/helloWorld.py
@@ -0,0 +1,57 @@
+import tweepy
+import AuthKeys as keys
+import re
+import time
+from TextRegenerator import trgnr
+from random import randrange
+
+
+consumer_key = keys.consumer_key
+consumer_secret = keys.consumer_secret
+access_token = keys.access_token
+access_token_secret = keys.access_token_secret
+
+auth = tweepy.OAuth1UserHandler(
+   consumer_key, consumer_secret, access_token, access_token_secret
+)
+
+api = tweepy.API(auth)
+
+#public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print(tweet.text)
+#api.update_status("Hello World!")
+
+mentions = api.mentions_timeline()
+
+listOfIDs = []
+for mentionInit in mentions:
+    listOfIDs.append(mentionInit.id)
+
+while(True):
+    mentions = api.mentions_timeline()
+    for mention in mentions:
+
+        statusID = mention.id
+        if statusID not in listOfIDs:
+            listOfIDs.append(statusID)
+
+            status = api.get_status(statusID, tweet_mode = "extended")
+            text = status.full_text.lower()
+            print(text)
+
+            #Do work on text here
+            text = re.sub(r"@([^\s]+)",'',text)
+            variationList = trgnr.generateStrVariations(text)
+            print(variationList)
+            output = "@" + str(mention.user.screen_name) + " " + variationList[randrange(0,len(variationList))]
+
+            #send out tweet
+            try:
+                api.update_status(output,in_reply_to_status_id = mention.id)
+            except:
+                print()
+    time.sleep(25)
+
+
+
